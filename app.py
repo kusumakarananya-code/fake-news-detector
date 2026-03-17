5a481db4e4ffd4abfb9d52a3b626b6b5786114e1
kusumakarananya-code <kusumakarananya@gmail.com>
2026-03-17 20:50:53 +0530

    fixed port binding for render

--- app.py
+++ app.py
@@ -23,4 +23,8 @@ def detect():
     
     return jsonify({"result":result})
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
